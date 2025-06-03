60d881878bdb2a8779ae43bc8921fcf415b42f25
ryancraker <ryan.craker98@gmail.com>
2025-06-03 16:27:19 -0600

    flex direction added to main

--- app/AppState.js
+++ app/AppState.js
@@ -22,7 +22,7 @@ class ObservableAppState extends EventEmitter {
     new Snack({
       name: "Not Milk",
       price: 3.75,
-      imgUrl: "https://images.unsplash.com/photo-1600788907416-456578634209?q=80&w=1950&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+      imgUrl: "https://images.unsplash.com/photo-1600788907416-456578634209?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
     })
   ]
 
